b0e8fa96f0cf8d937dfb9fdec09fabd756b39bd6
S-H JIN <jinsuhyeon00@naver.com>
2021-05-29 22:02:04 +0900

    진수현 합치기

---
+++ app/src/main/java/com/example/pet_dairy/Writing.java
@@ -0,0 +1,20 @@
+package com.example.pet_dairy;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Writing extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing);
+
+    }
+}
